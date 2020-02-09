247c33eb8393731959faf3589eed76018abda477
Raoni Sian <raonisiann@live.com>
2020-02-09 20:17:27 +0000

    Adding the stack on context

--- src/core/context.c
+++ src/core/context.c
@@ -6,8 +6,10 @@
 struct e_context *context_create(unsigned int sym_init_size){
     struct e_context *new_ctxt = (struct e_context *)memm_alloc(sizeof(struct e_context));
     // Create and allocate a hashtable to hold the context symbols (references and function def).
-    // Hashtables are dynamically sized and this is a initial value only.
+    // Hashtables are dynamically sized and this is a initial value only.    
     new_ctxt->symbols = ht_init(sym_init_size);
+    // hardcoding stack size now... 
+    new_ctxt->stack = stack_init(10);
     new_ctxt->ret_val = NULL;
     new_ctxt->sig_ret = 0;
     return new_ctxt;
