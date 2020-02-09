79ff477b5090c7a4900c23cebfacc906b823aad8
Raoni Sian <raonisiann@live.com>
2020-02-09 19:51:46 +0000

    Context implementation

--- src/core/include/context.h
+++ src/core/include/context.h
@@ -8,9 +8,17 @@
 // Also provides an way to exchange information 
 // between different context with "ret_val"
 struct e_context {
-    hashtable *local_symbols;   
+    hashtable *symbols;   
 	struct e_value *ret_val;	
 	unsigned int sig_ret;
 };
+
+struct e_context *context_create(unsigned int sym_init_size);
+
+// hold the global symbols. Local context are created dynamically 
+// on function calls
+struct e_context *context_europa_global;
+
+#define GLOBAL_CTXT context_europa_global
 
 #endif
