1585ac3fb2f01eb1cac8829c06bf0396e0cd78db
Raoni Sian <raonisiann@live.com>
2020-03-09 23:43:19 +0000

    References from "LEX_INPUT" are now useless. Also the logic of open a file was moved to factory method.

--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@
 #include "context.h"
 #include "functions.h"
 
-extern FILE *LEX_INPUT;
 extern char europa_shell_mode;
 
 int main(int argc, char *argv[]){
@@ -17,27 +16,21 @@ int main(int argc, char *argv[]){
         europa_debug_init();
         DEBUG_OUTPUT("\n\nSTART DEBUG\n=================================\n")
     #endif 
-    
-// setting the default to stdin 
-    char *input_file_name;
-    LEX_INPUT = LEX_INPUT_DEFAULT;
+    	
+	eu_init();
     // create the global context
     GLOBAL_CTXT = context_create(100);
     
-    functions_init();       
-    lex_init();
+    functions_init();           
 
 	if(argc > 1){
-        // parse the language as non-interactive mode 
-		if((LEX_INPUT = fopen(argv[1], "r")) != NULL){
-            // ues a file for input 
-			input_file_name = argv[1];                   
-		}else{
-            EUROPA_ERROR("Unable to open file '%s'\n", argv[1]);
-        }           
+		push_to_include_stack(factory_file_desc(argv[1]));    
+		lex_init();		
         eu_lang(); 
 	}else{  
         europa_shell_mode = 1;    
+		push_to_include_stack(factory_file_desc(EU_SHELL_FILE_NAME));
+		lex_init();		
         // parse the language as shell mode  
         eu_shell();
     }  
