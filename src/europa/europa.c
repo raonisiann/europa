a593c0c2e5945515fd0d215dd55a6e407f3bddd9
Raoni Sian <raonisiann@live.com>
2020-03-09 22:47:31 +0000

    new functions to manager file include stack

--- src/europa/europa.c
+++ src/europa/europa.c
@@ -1,3 +1,59 @@
-#include <europa.h>
+#include <string.h>
+#include "europa.h"
+#include "stack.h"
+#include "memm.h"
+#include "europa_error.h"
 
 char europa_shell_mode = 0;
+e_stack *eu_include_stack = NULL;
+struct eu_file_desc *eu_current_include_file = NULL;
+
+void eu_init(){
+
+	eu_include_stack = stack_init(EU_INCLUDE_MAX_DEPTH);
+}
+
+void push_to_include_stack(struct eu_file_desc *file){
+	
+	stack_push(eu_include_stack, (void *)file);
+	eu_current_include_file = file; 
+}
+
+struct eu_file_desc *pop_from_include_stack(){
+	struct eu_file_desc *ret = NULL;
+	ret = (struct eu_file_desc *)stack_pop(eu_include_stack);
+	// update the current include file with 
+	// the top of the stack 
+	eu_current_include_file = (struct eu_file_desc *)stack_peek(eu_include_stack);
+	return ret;
+}
+
+int get_include_stack_size(){
+	
+	return EU_INCLUDE_MAX_DEPTH - eu_include_stack->free; 
+}
+
+struct eu_file_desc *get_current_file_desc(){
+	return eu_current_include_file;
+}
+
+struct eu_file_desc *factory_file_desc(char *fname){
+	struct eu_file_desc *newfile = (struct eu_file_desc *)memm_alloc(sizeof(struct eu_file_desc));
+	newfile->line_num = 0; 
+	newfile->char_pos = 0; 
+	newfile->file_name = fname; 
+	if(strncmp(fname, EU_SHELL_FILE_NAME, sizeof(EU_SHELL_FILE_NAME) / (sizeof(char))) == 0){
+		// shell mode. Setting input to 'stdin'
+		newfile->fd = stdin;
+	}else{
+		// TODO: 
+		// need to perform some tests on the file before open it 
+		newfile->fd = fopen(fname, "r");
+		if(newfile->fd == NULL){
+			EUROPA_ERROR("Unable to open file '%s'\n", fname);
+			return NULL; 
+		}
+	}	
+	return newfile; 
+}
+
--- src/europa/include/europa.h
+++ src/europa/include/europa.h
@@ -1,7 +1,10 @@
+#ifndef I_EUROPA_H
+#define I_EUROPA_H
+#include <stdio.h>
+#include "stack.h"
+
 #define EU_VERSION "0.1"
 #define EU_BUILT_DATE __TIMESTAMP__
-
-char europa_shell_mode;
 
 #if DEBUG
 #define EU_DEBUG_INFO "with debug flag"
@@ -28,4 +31,35 @@ char europa_shell_mode;
     #define EU_BUILT_OS "Mac"
 #else 
     #define EU_BUILT_OS "Unkown"
+#endif
+
+// max depth allowed for include files
+#define EU_INCLUDE_MAX_DEPTH 15
+
+#define EU_SHELL_FILE_NAME "***EUROPA_SHELL***"
+
+
+struct eu_file_desc {
+	unsigned int line_num; 
+	unsigned int char_pos;
+	char *file_name; 
+	FILE *fd; 
+};
+
+struct e_stack *eu_include_stack_init();
+void push_to_include_stack(struct eu_file_desc *file);
+struct eu_file_desc *pop_from_include_stack();
+int get_include_stack_size();
+struct eu_file_desc *get_current_file_desc();
+struct eu_file_desc *factory_file_desc(char *fname);
+
+void eu_init();
+
+
+#define CURRENT_FILE_STRUCT get_current_file_desc()
+#define CURRENT_FILE_NAME CURRENT_FILE_STRUCT->file_name
+#define CURRENT_LINE_NUM CURRENT_FILE_STRUCT->line_num
+#define CURRENT_CHAR_POS CURRENT_FILE_STRUCT->char_pos
+#define CURRENT_FD CURRENT_FILE_STRUCT->fd
+
 #endif
