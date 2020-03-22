7fab036455ca9550639b7e1c05ca5912c222d9b4
Raoni Sian Das Neves <raoni@amazon.com>
2020-03-22 23:06:11 +0000

    introducing lex_context to manager captured tokens, current char, line num and char pos. Also the structs tk_list and tk_node to handle the token list. Lex init method is no long er required with lex_context

--- src/europa/include/lexer.h
+++ src/europa/include/lexer.h
@@ -19,7 +19,8 @@
 #define LEX_SCAPE_CHAR          '\\'
 
 #define TOKEN lex_tk->class
-
+#define CURRENT_LINE_NUM lex_cur_ctxt->line_num
+#define CURRENT_CHAR_POS lex_cur_ctxt->char_pos
 
 typedef enum {
     undefined = -1,   
@@ -64,6 +65,28 @@ typedef enum {
 	usecmd
 } lex_token_class;
 
+struct tk_list {
+	int size;
+	struct tk_node *first;
+	struct tk_node *last;
+	struct tk_node *cur;
+};
+
+struct tk_node {
+	struct tk_node *next;
+	struct tk_node *prev;
+	struct lex_token *tk;
+};
+
+// lex_tk : tk_list->cur->tk
+struct lex_context {
+	char lex_cur_ch;
+	char lex_prev_ch;
+	unsigned int line_num;
+	unsigned int char_pos;
+	struct tk_list *tk_list;
+};
+
 // Struct to store a lex token 
 // Must be filled with a class (from one of the above)
 // and a value with the raw string captured (if necessary)
@@ -83,6 +106,8 @@ struct reserved_word {
 // store the current lex token 
 struct lex_token *lex_tk;
 
+// current lex context 
+struct lex_context *lex_cur_ctxt;
 
 // Current char for lexer. It's updated by
 // lex_next_char function
@@ -144,12 +169,18 @@ void lex_error(const char *error);
 void lex_ignore_white_spaces();
 
 void lex_clear_capture_buffer(char *buffer, unsigned int size);
-void lex_init();
 
 void lex_verbose(char *s, ...);
 
 char *lex_token_to_text(int tk_class);
 
 void lex_reset_state();
+void lex_switch_context(struct lex_context *ctxt);
+
+struct lex_context *lex_create_context();
+struct tk_list *tk_create_list();
+struct tk_node *tk_create_node();
+
+void tk_add_node(struct tk_list *list, struct lex_token *tk);
 
 #endif
